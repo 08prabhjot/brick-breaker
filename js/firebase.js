f397533f7a5de6d29a70ae83d878be718413b24a
08prabhjot <prabhjot3737@gmail.com>
2023-03-01 18:41:24 +0700

    try add firebase

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANjXg9ighDIyS7xgYEIUfg65Bcdzv396I",
+    authDomain: "test-d075e.firebaseapp.com",
+    databaseURL: "https://test-d075e-default-rtdb.firebaseio.com",
+    projectId: "test-d075e",
+    storageBucket: "test-d075e.appspot.com",
+    messagingSenderId: "1003451507207",
+    appId: "1:1003451507207:web:84804bcf5664fb189bc0a2",
+    measurementId: "G-5FLQLC7CVY"
+};
+
